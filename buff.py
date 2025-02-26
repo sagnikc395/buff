bcf04c0396e549f522b28ff2177682e5b22275bc
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-02-27 00:59:55 +0530

    src: added state machine and tests

--- buff.py
+++ buff.py
@@ -49,4 +49,63 @@ class InfiniteTape:
     def set(self,number: int) -> None:
         self._tape[self._cursor] = number
 
+# state machine:
+# this takes a program and inputs and we can go and step through the program
 
+# we can just count the brackets and move within the program until we have reached the matching bracket
+
+class StateMachine:
+    def __init__(self,program: str, inputs: list[int]) -> None:
+        self._program = program
+        self._inputs = inputs
+        self._tape = InfiniteTape()
+
+        self._pc = 0
+        self._outputs = []
+
+    def run(self) -> list[int]:
+        while self._pc != len(self._program):
+            self.step()
+        return self._outputs
+    
+    def _goto_bracket(self,forward: bool) -> None:
+        curr_bracket = "[" if forward else "]"
+        target_bracket = "]" if forward else "["
+
+        increment = 1 if forward else -1
+        bracket_counter = 0
+
+        while True:
+            instruction = self._program[self._pc]
+            if instruction == curr_bracket:
+                bracket_counter += 1
+            elif instruction == target_bracket:
+                bracket_counter -= 1
+                if bracket_counter == 0:
+                    break
+            self._pc += increment
+
+    def step(self) -> None:
+        instruction = self._program[self._pc]
+        match(instruction):
+            case "+":
+                self._tape.increment()
+            case "-":
+                self._tape.decrement()
+            case ">":
+                self._tape.right()
+            case "<":
+                self._tape.left()
+            case ",":
+                self._tape.set(self._inputs.pop(0))
+            case ".":
+                self._outputs.append(self._tape.get())
+            case "[":
+                if self._tape.is_zero():
+                    self._goto_bracket(forward=True)
+            case "]":
+                if not self._tape.is_zero():
+                    self._goto_bracket(forward=False)
+
+        self._pc += 1
+        
